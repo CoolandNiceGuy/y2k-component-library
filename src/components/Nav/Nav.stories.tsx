5fd529dd7cb3d7856b6aa2308ac6b61abc977811
CoolandNiceGuy <42616124+CoolandNiceGuy@users.noreply.github.com>
2023-02-12 14:02:38 -0600

    Revert "facebook"
    This reverts commit cc615fb80ea826d9d8e4342602183ab3c1e8514d.

---
+++ src/components/Nav/Nav.stories.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Nav from './Nav'
+
+// [{linkDestination: '', icon: 'house'},{linkDestination: '', icon: 'bars'}, {linkDestination: '', icon: 'user'}, {linkDestination: '', icon: 'code'}, {linkDestination: '', icon: 'terminal'}, {linkDestination: '', icon: 'gears'}, {linkDestination: '', icon: 'facebook'}, {linkDestination: '', icon: 'twitter'},{linkDestination: '', icon: 'github'}, {linkDestination: '', icon: 'linkedin'}, {linkDestination: '', icon: 'tiktok'}, {linkDestination: '', icon: 'twitch'}]
+
+export default {
+  title: 'Nav',
+  component: Nav,
+  argTypes: {
+    color: {control: 'radio', options: ['blue', 'red', 'green']},
+    direction: {control: 'radio', options: ['vertical', 'horizontal']},
+  }
+} as ComponentMeta<typeof Nav>;
+
+const Template: ComponentStory<typeof Nav> = (args) => <Nav {...args} />;
+
+export const Vertical = Template.bind({});
+Vertical.args = {
+  color: 'red',
+  direction: 'vertical',
+  buttons: [{linkDestination: '', icon: 'bars'},{linkDestination: '', icon: 'terminal'},{linkDestination: '', icon: 'twitch'}]
+}
+
+export const Horizontal = Template.bind({});
+Horizontal.args = {
+  color: 'green',
+  direction: 'horizontal',
+  buttons: [{linkDestination: '', icon: 'tiktok'},{linkDestination: '', icon: 'house'},{linkDestination: '', icon: 'gears'}]
+}
