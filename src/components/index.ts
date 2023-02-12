1cc786b8edb9d95b4a3f870710776215fba3121e
CoolandNiceGuy <42616124+CoolandNiceGuy@users.noreply.github.com>
2023-02-12 17:07:52 +0000

    Deploying to master from @ CoolandNiceGuy/y2k-component-library@e33302ee49090d079a7bb0881a093d65ed151f33 🚀

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Button } from "./Button";
-export { default as Image } from "./Image";
-export { default as List } from "./List";
-export { default as Nav } from "./Nav";
-export { default as Section } from "./Section";
-export { default as Snippet } from "./Snippet";
-export {default as NavItem} from './NavItem'
